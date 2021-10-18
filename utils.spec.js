5495d6c59fd9eb7c42fca81b9e940f66724006b4
ronaldoyoung <ronaldoyoung@gmail.com>
2021-10-18 16:27:07 +0900

    should

--- utils.spec.js
+++ utils.spec.js
@@ -1,9 +1,9 @@
 const utils = require('./utils')
-const assert = require('assert');
+const should = require('should');
 
 describe('utils.js 모듈의 capitalize() 함수는', () => {
   it('문자열의 첫번째 문자를 대문자로 반환', () => {
     const result = utils.capitialize('hello');
-    assert.equal(result, 'Hello');
+    result.should.be.equal('Hello')
   })
 })
