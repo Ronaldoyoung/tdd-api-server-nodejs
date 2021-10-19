1da79e125843e25d4d89490eefa1a10a1eec1ec1
ronaldoyoung <ronaldoyoung@gmail.com>
2021-10-19 16:29:39 +0900

    테스트 환경 개선

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const morgan = require('morgan');
 const app = express();
 let user = require('./api/user');
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/users', user);
 
-app.listen(3000, () => {
-  console.log('Server is running');
-});
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000!');
+// });
 
 module.exports = app;
