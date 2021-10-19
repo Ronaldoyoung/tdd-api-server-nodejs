39205e497054a1372efa4a428c0367f0a14e8dce
ronaldoyoung <ronaldoyoung@gmail.com>
2021-10-19 13:56:20 +0900

    사용자 삭제 api 성공시

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ app.get('/users/:id', (req, res) => {
   res.json(user);
 });
 
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  users = users.filter(user => user.id !== id);
+  res.status(204).end();
+})
+
 app.listen(3000, () => {
   console.log('Server is running');
 });
