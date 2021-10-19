d47438d00d4018e7c81a3af90af922cff19916e5
ronaldoyoung <ronaldoyoung@gmail.com>
2021-10-19 13:41:05 +0900

    사용자 조회 api 성공시

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.get('/users', (req, res) => {
   res.json(users.slice(0, limit));
 });
 
+app.get('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const user = users.filter(user => user.id === id)[0];
+  res.json(user);
+});
+
 app.listen(3000, () => {
   console.log('Server is running');
 });
