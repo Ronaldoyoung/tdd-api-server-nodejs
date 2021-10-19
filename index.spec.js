7d11aa9ad8637744f008872b0cba563413e534c6
ronaldoyoung <ronaldoyoung@gmail.com>
2021-10-19 13:29:23 +0900

    사용자 모록 조회 api 테스트 코드 만들기 2

--- index.spec.js
+++ index.spec.js
@@ -11,6 +11,22 @@ describe('GET /users는', () => {
           res.body.should.be.instanceOf(Array);
           done();
         })
+    });
+    it('최대 limit 갯수만큼 응답한다.', (done) => {
+      request(app)
+        .get('/users?limit=2')
+        .end((err, res) => {
+          res.body.should.be.lengthOf(2)
+          done();
+        })
+    })
+  })
+  describe('실패 시', () => {
+    it('limit이 숫자형이 아니면 400을 응답한다.', (done) => {
+      request(app)
+        .get('/users?limit=two')
+        .expect(400)
+        .end(done)
     })
   })
 })
